31d1576b4a8e0ebd055934d19561d29e74d297c9
Dario Scalia <darioscalia99@gmail.com>
2025-04-01 07:21:29 +0200

    create an http server

---
+++ server.js
@@ -0,0 +1,16 @@
+import { log } from "node:console";
+import { createServer, request } from "node:http";
+
+const server = createServer((request, response) => {
+    console.log("request recive, nice!");
+
+    response.statusCode = 200;
+
+    response.setHeader("Content-Type", "text/html");
+
+    response.end("<html><body><h1>Welcome to my page with server</h1></body></html>")
+})
+
+server.listen(3000, () => {
+    console.log(`My own server is running at http://localhost:3000`);
+})
